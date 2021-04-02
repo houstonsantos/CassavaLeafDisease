687146ee56744deb0c25ade13ee757ccd6ab0da1
Houston Santos <houston_santos@hotmail.com>
2021-04-02 13:54:43 -0300

    Add random_eraser

--- random_eraser.py
+++ random_eraser.py
@@ -1,8 +1,13 @@
 import numpy as np
 
-def get_random_eraser(p=0.5, s_l=0.02, s_h=0.4, r_1=0.3, r_2=1/0.3, v_l=0, v_h=255):
+
+def get_random_eraser(p=0.5, s_l=0.02, s_h=0.4, r_1=0.3, r_2=1/0.3, v_l=0, v_h=255, pixel_level=False):
     def eraser(input_img):
-        img_h, img_w, _ = input_img.shape
+        if input_img.ndim == 3:
+            img_h, img_w, img_c = input_img.shape
+        elif input_img.ndim == 2:
+            img_h, img_w = input_img.shape
+
         p_1 = np.random.rand()
 
         if p_1 > p:
@@ -19,8 +24,15 @@ def get_random_eraser(p=0.5, s_l=0.02, s_h=0.4, r_1=0.3, r_2=1/0.3, v_l=0, v_h=2
             if left + w <= img_w and top + h <= img_h:
                 break
 
-        c = np.random.uniform(v_l, v_h)
-        input_img[top:top + h, left:left + w, :] = c
+        if pixel_level:
+            if input_img.ndim == 3:
+                c = np.random.uniform(v_l, v_h, (h, w, img_c))
+            if input_img.ndim == 2:
+                c = np.random.uniform(v_l, v_h, (h, w))
+        else:
+            c = np.random.uniform(v_l, v_h)
+
+        input_img[top:top + h, left:left + w] = c
 
         return input_img
 
